7d2976c3ec2ab47a54e806278a893d294004b42f
Bob <bob.e.baxter@gmail.com>
2019-05-20 21:26:54 -0500

    authentication works

---
+++ src/javascripts/helpers/data/authData.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+
+const authDiv = document.getElementById('auth');
+const birthdayDiv = document.getElementById('birthday');
+const birthdayNavbar = document.getElementById('navbar-button-birthday');
+const authNavbar = document.getElementById('navbar-button-auth');
+const logoutNavbar = document.getElementById('navbar-button-logout');
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      authDiv.classList.add('hide');
+      birthdayDiv.classList.remove('hide');
+      birthdayNavbar.classList.remove('hide');
+      authNavbar.classList.add('hide');
+      logoutNavbar.classList.remove('hide');
+    } else {
+      authDiv.classList.remove('hide');
+      birthdayDiv.classList.add('hide');
+      birthdayNavbar.classList.add('hide');
+      authNavbar.classList.remove('hide');
+      logoutNavbar.classList.add('hide');
+    }
+  });
+};
+
+export default { checkLoginStatus };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,19 @@
 import firebase from 'firebase/app';
 
-import '../styles/main.scss';
 import 'bootstrap';
+import '../styles/main.scss';
 
 import auth from './components/auth/auth';
 import birthday from './components/birthday/birthday';
+import MyNavbar from './components/MyNavbar/MyNavbar';
+import authData from './helpers/data/authData';
 
 import apiKeys from './helpers/apiKeys.json';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  MyNavbar.navbarEvents();
+  authData.checkLoginStatus();
   auth.printAuth();
   birthday.printBirthday();
 };
