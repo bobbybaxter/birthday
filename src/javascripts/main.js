f21e667aa79a80d85b55fb4425700933ce75786e
Bob <bob.e.baxter@gmail.com>
2019-05-20 19:55:02 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,15 @@
+import firebase from 'firebase/app';
+
 import '../styles/main.scss';
 import 'bootstrap';
+
 import auth from './components/auth/auth';
 import birthday from './components/birthday/birthday';
 
+import apiKeys from './helpers/apiKeys.json';
+
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.printAuth();
   birthday.printBirthday();
 };
